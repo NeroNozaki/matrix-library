73c34ad99e96b82438ad76286e9ab1140f6f21ca
shinmonzip <natanshinmon@gmail.com>
2026-09-15 18:04:58 -0300

    fix: validate matrix dimensions in constructor to prevent illegal arguments

--- src/matrix/Matrix.java
+++ src/matrix/Matrix.java
@@ -22,7 +22,11 @@ public class Matrix {
 
     }
     public Matrix(int rows, int columns) {
-        this.data = new double[rows][columns];
+        if (rows > 0 || columns > 0) {
+            this.data = new double[rows][columns];
+        } else {
+            throw new IllegalArgumentException("Invalid matrix dimensions.");
+        }
     }
 
     public Matrix(Matrix matrix) {
